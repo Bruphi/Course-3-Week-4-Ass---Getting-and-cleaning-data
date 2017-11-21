80588dc654f77db5d293d16930b15afa35bfa060
Bruphi <32820411+Bruphi@users.noreply.github.com>
2017-11-21 13:09:56 +0200

    Create ReadmeExplain.m

---
+++ ReadmeExplain.m
@@ -0,0 +1,25 @@
+## Getting and Cleaning Data
+## Course 3 Week 4
+
+The project has been created to finsh the Coursera Course 3 - Getting and cleaning data
+
+Data was recieved from https://www.coursera.org/learn/data-cleaning/peer/FIZtT/getting-and-cleaning-data-course-proje
+The data was recorded using accelerometers from the Samsung Galaxy S smartphone by 30 persons  doing 6 diffrent acitivities.
+File was unzipped and cleaned using R.
+A text file tidy data set was created giving the summarized results
+
+
+## Code Explained
+The two data sets training and test was combined and then only certain variables was taken out od the combined data set.
+The average of these extracted datasets was then calculated and reported in a neat data set calles tidy data with better named collums.
+
+The following instruction was followed in order to produce the tidy data set.
+
+
+Merges the training and the test sets to create one data set.
+Extracts only the measurements on the mean and standard deviation for each measurement.
+Uses descriptive activity names to name the activities in the data set
+Appropriately labels the data set with descriptive variable names.
+From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+The tidy data set was then uploaded/pushed to git hub and submitted to be reviewed by our fellow peers.
